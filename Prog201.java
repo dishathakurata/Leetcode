85930ea00a7264efb4257b845ec5a751405ac97c
Disha Thakurata <146114938+dishathakurata@users.noreply.github.com>
2024-02-21 21:26:24 +0530

    Add files via upload

---
+++ Prog201.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+//201. Bitwise AND of numbers range
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int count = 0;
+
+        while(left != right) {
+            left = left >> 1;
+            right = right >> 1;
+            count++;
+        }
+
+        return left << count;
+    }
+}
