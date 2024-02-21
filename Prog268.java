9fae87531e53641625fe4665d302f19adbcc0879
Disha Thakurata <146114938+dishathakurata@users.noreply.github.com>
2024-02-21 21:24:53 +0530

    Update Prog268.java

--- Prog268.java
+++ Prog268.java
@@ -4,21 +4,21 @@ package leetcode;
 
 class Solution {
     public int missingNumber(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
         int t;
         
-        for(int i=0;i<n-1;i++) {
-            for(int j=i+1;j<n;j++) {
-                if(nums[i]>nums[j]) {
-                    t=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=t;
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(nums[i] > nums[j]) {
+                    t = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = t;
                 }
             }
         }
 
-        for(int k=0;k<n;k++) {
-            if(nums[k]!=k) {
+        for(int k = 0; k < n; k++) {
+            if(nums[k] != k) {
                 return k;
             }
         }
