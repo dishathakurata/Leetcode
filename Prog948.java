9d6ca29e798bc432c63c4ffd6036ce83be8e2abc
Disha Thakurata <146114938+dishathakurata@users.noreply.github.com>
2024-03-04 22:17:14 +0530

    Add files via upload

---
+++ Prog948.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+//948. Bag of tokens
+
+import java.util.Arrays;
+
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+
+        int left = 0;
+        int right = tokens.length - 1;
+        int score = 0;
+        int maxScore = 0;
+
+        while(left <= right) {
+            if(power >= tokens[left]) {
+                power -= tokens[left++];
+                score++;
+                maxScore = Math.max(score, maxScore);
+            }
+
+            else if(score > 0) {
+                power += tokens[right--];
+                score--;
+                maxScore = Math.max(score, maxScore);
+            }
+
+            else {
+                break;
+            }
+        }
+
+        return maxScore;
+    }
+}
