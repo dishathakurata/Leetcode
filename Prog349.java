d40e9ee2e36d6d4f9755be9f4f56d52951d8a350
Disha Thakurata <146114938+dishathakurata@users.noreply.github.com>
2024-03-10 10:36:50 +0530

    Add files via upload

---
+++ Prog349.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+//349. Intersection of two arrays
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for(int n1: nums1) {
+            set1.add(n1);
+        }
+
+        for(int n2: nums2) {
+            if(set1.contains(n2)) {
+                set2.add(n2);
+            }
+        }
+
+        int k = 0;
+        int[] result = new int[set2.size()];
+        for(int s: set2) {
+            result[k] = s;
+            k++;
+        }
+
+        return result;
+    }
+}
